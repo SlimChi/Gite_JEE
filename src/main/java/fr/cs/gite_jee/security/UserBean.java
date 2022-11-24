1980f2310e8af798a06d31743e5161888f157c7a
redouane <redouane.takdjerad@gmail.com>
2022-11-24 15:09:46 +0100

    details front

--- src/main/java/fr/cs/gite_jee/security/UserBean.java
+++ src/main/java/fr/cs/gite_jee/security/UserBean.java
@@ -18,6 +18,34 @@ import java.util.Date;
 @RequestScoped
 public class UserBean implements Serializable {
 
+
+
+    @Inject
+    ApplicationBean applicationBean;
+    String login;
+    String email;
+    String password;
+
+
+    public void Creer() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE,10);
+        Date expiration = calendar.getTime();
+        String url = String.format("/%s?c=%s&e=%s&p=%s&d=%s",
+                login,
+                SecurityTools.checksum(login+email),
+                email,
+                applicationBean.passwordHash(password),
+                new SimpleDateFormat("dd-MM-yy-HH:mm:ss").format(expiration));
+        String urlEncode = SecurityTools.encrypt(url);
+        String valideUrl = applicationBean.getAbsolutePath() + "/confirm.jsf?compte=" + urlEncode;
+        StringBuilder body = new StringBuilder("Veuillez cliquer le lien");
+        body.append(valideUrl);
+        Email.sendEmail(email,"Confirmation",body.toString());
+
+
+    }
+
     public ApplicationBean getApplicationBean() {
         return applicationBean;
     }
@@ -49,31 +77,4 @@ public class UserBean implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
-
-    @Inject
-    ApplicationBean applicationBean;
-    String login;
-    String email;
-    String password;
-
-
-    public void Creer() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MINUTE,10);
-        Date expiration = calendar.getTime();
-        String url = String.format("/%s?c=%s&e=%s&p=%s&d=%s",
-                login,
-                SecurityTools.checksum(login+email),
-                email,
-                applicationBean.passwordHash(password),
-                new SimpleDateFormat("dd-MM-yy-HH:mm:ss").format(expiration));
-        String urlEncode = SecurityTools.encrypt(url);
-        String valideUrl = applicationBean.getAbsolutePath() + "/confirm.jsf?compte=" + urlEncode;
-        StringBuilder body = new StringBuilder("Veuillez cliquer le lien");
-        body.append(valideUrl);
-        Email.sendEmail(email,"Confirmation",body.toString());
-
-
-    }
-
 }
